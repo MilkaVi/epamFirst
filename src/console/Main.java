2f4bd39501c70c76dfdf44808d73d50dc6941571
nikolaycswow@mail.ru <nikolaycswow@mail.ru>
2021-04-07 20:41:25 +0300

    format

--- src/console/Main.java
+++ src/console/Main.java
@@ -2,13 +2,15 @@ package console;
 
 import model.tax.Tax;
 
+import java.util.Arrays;
 import java.util.TreeSet;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
         TreeSet<Tax> tasx = new TaxReader().read();
         for (Tax t : tasx) {
-            System.out.println(t + ", final tax = " + t.getTax());
+            System.out.println(t + ", final tax = " + String.format("%.3g", t.getTax()));
         }
     }
 }
